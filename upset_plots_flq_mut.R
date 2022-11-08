5aba7fd74424cd05224525da097a406c02c19add
Iana Amke <ianaamke@gmail.com>
2022-11-08 21:50:37 +0000

    Add AST breakpoints, summary plots and function
    Added phenotype based on CLSI and EUCAST breakpoints
    
    Added mutations extracted via BLASTP including mutations not
    previously found in Kleborate
    
    Added summaries of resistant strains with no gyrA/parC mutations
    
    Made UpSet plots of strains with acquired fluoroquinolone resistance
    
    Wrote function to separate grouped acquired mutations into individual
    acquired mutations

--- upset_plots_flq_mut.R
+++ upset_plots_flq_mut.R
@@ -10,7 +10,35 @@ antibiogram <- read.delim(file = "antibiogram_combined.tsv",
 # filter for ciprofloxacin
 cipro_antibiogram <- antibiogram %>%
   filter(Antibiotic == "ciprofloxacin") %>%
-  select(strain:ST, Flq_mutations, Antibiotic:Measurement.Round)
+  select(strain:ST, Flq_mutations, Flq_acquired, Omp_mutations, Antibiotic:Measurement.Round) %>%
+  mutate(Resistance.phenotype = case_when(grepl('MIC.*$', Laboratory.Typing.Method) & Measurement <= 0.25 ~ "susceptible",
+                                          grepl('MIC.*$', Laboratory.Typing.Method) & Testing.standard == "EUCAST" & 
+                                            Measurement > 0.5 ~ "resistant",
+                                          grepl('MIC.*$', Laboratory.Typing.Method) & Testing.standard == "CLSI" & 
+                                            Measurement >= 1 ~ "resistant",
+                                          Laboratory.Typing.Method == "Disk diffusion" & Testing.standard == "EUCAST" & 
+                                            Measurement >= 25 ~ "susceptible",
+                                          Laboratory.Typing.Method == "Disk diffusion" & Testing.standard == "EUCAST" & 
+                                            Measurement < 22 ~ "resistant",
+                                          Laboratory.Typing.Method == "Disk diffusion" & Testing.standard == "CLSI" & 
+                                            Measurement >= 26 ~ "susceptible",
+                                          Laboratory.Typing.Method == "Disk diffusion" & Testing.standard == "CLSI" & 
+                                            Measurement <= 21 ~ "resistant",
+                                          TRUE ~ "intermediate"))
+
+#write_delim(cipro_antibiogram, file = "cipro_antibiogram.tsv", delim = "\t")
+
+# combine antibiogram with mutations not in kleborate data file
+no_mut_kleborate <- read.delim(file = "cipro_res_kleb/mutations_not_in_kleborate.tsv",
+                               header = TRUE, sep = "\t")
+
+cipro_antibiogram <- cipro_antibiogram %>%
+  left_join(no_mut_kleborate, by = c("strain")) %>%
+  mutate(Flq_mutations = case_when(Flq_mutations.y %in% NA ~ Flq_mutations.x, 
+                                   Flq_mutations.x == "-" & Flq_mutations.y %in% NA ~ Flq_mutations.x,
+                                   TRUE ~ Flq_mutations.y)) %>%
+  select(- c(Flq_mutations.x, Flq_mutations.y)) %>%
+  relocate(Flq_mutations, .after = ST) 
 
 # SUMMARIES
 # MIC (broth dilution)
@@ -40,26 +68,6 @@ diskdiff_cipro_antibiogram <- cipro_antibiogram %>%
 
 testing_stds_diskdiff <- diskdiff_cipro_antibiogram %>%
   testing_std()
-
-# VIOLIN AND UPSET PLOTS FOR MUTATION PATTERNS
-# EUCAST/CLSI cut-offs added to infer phenotype
-# MIC broth 
-micbroth_cipro_antibiogram %>%
-  upset_plot_mic() +
-  labs(title = "Fluoroquinolone resistance mutations for MIC (broth dilution)")
-
-# MIC agar 
-micagar_cipro_antibiogram %>%
-  upset_plot_mic() +
-  labs(title = "Fluoroquinolone resistance mutations for MIC (agar dilution)")
-
-# MIC etest - **only one entry
-#micetest_cipro_antibiogram %>%
-  #upset_plot()
-
-# Disk diffusion
-diskdiff_cipro_antibiogram %>%
-  upset_plot_diskdiff()
 
 # VIOLIN AND UPSET PLOTS FOR INDIVIDUAL MUTATION PATTERNS
 # separate Flq_mutations into individual mutations
@@ -94,43 +102,62 @@ diskdiff_cipro_antibiogram_sep %>%
 # MIC broth
 micbroth_no_mut <- micbroth_cipro_antibiogram_sep %>%
   filter(Flq_mutations == "-") %>%
-  mutate(Resistance.phenotype = case_when(Measurement <= 0.25 ~ "susceptible",
-                                          Testing.standard == "EUCAST" & Measurement > 0.5 ~ "resistant",
-                                          Testing.standard == "CLSI" & Measurement >= 1 ~ "resistant",
-                                          TRUE ~ "intermediate")) %>%
   filter(Resistance.phenotype == "resistant")
 
 # MIC agar
 micagar_no_mut <- micagar_cipro_antibiogram_sep %>%
   filter(Flq_mutations == "-") %>%
-  mutate(Resistance.phenotype = case_when(Measurement <= 0.25 ~ "susceptible",
-                                          Measurement > 0.5 ~ "resistant",
-                                          TRUE ~ "intermediate")) %>%
   filter(Resistance.phenotype == "resistant")
 
 # MIC E-test
 micetest_no_mut <- micetest_cipro_antibiogram_sep %>%
   filter(Flq_mutations == "-") %>%
-  mutate(Resistance.phenotype = case_when(Measurement <= 0.25 ~ "susceptible",
-                                          Measurement > 0.5 ~ "resistant",
-                                          TRUE ~ "intermediate")) %>%
   filter(Resistance.phenotype == "resistant")
 
 # Disk diffusion
 diskdiff_no_mut <- diskdiff_cipro_antibiogram_sep %>%
   filter(Flq_mutations == "-") %>%
-  mutate(Resistance.phenotype = case_when(Testing.standard == "EUCAST" & Measurement >= 25 ~ "susceptible",
-                                          Testing.standard == "EUCAST" & Measurement < 22 ~ "resistant",
-                                          Testing.standard == "CLSI" & Measurement >= 26 ~ "susceptible",
-                                          Testing.standard == "CLSI" & Measurement <= 21 ~ "resistant",
-                                          TRUE ~ "intermediate")) %>%
   filter(Resistance.phenotype == "resistant")
 
 # join all dataframes
 cipro_res_no_mut <- rbind(micbroth_no_mut, micagar_no_mut, micetest_no_mut, diskdiff_no_mut)
 
-write_delim(cipro_res_no_mut, file = "cipro_res_no_mutations.tsv", delim = "\t")
+write_delim(cipro_res_no_mut, file = "cipro_res_no_mutations_2.tsv", delim = "\t")
 
+# VIOLIN AND UPSET PLOTS FOR ACQUIRED RESISTANCE PATTERNS OF STRAINS WITH NO MUTATIONS
+cipro_res_no_mut <- cipro_antibiogram %>%
+  filter(Flq_mutations == "-" & Resistance.phenotype == "resistant")
+  
+# separate acquired_mutations into individual mutations and create upset plots
+# MIC broth
+micbroth_cipro_acquired_sep <- cipro_res_no_mut %>%
+  filter(Laboratory.Typing.Method == "MIC (broth dilution)") %>%
+  sep_acq_mutations()
+
+micbroth_cipro_acquired_sep %>%
+  upset_plot_mic_acq() +
+  labs(title = "MIC (broth dilution) measurements for acquired fluoroquinolone resistance")
+
+# MIC agar
+micagar_cipro_acquired_sep <- cipro_res_no_mut %>%
+  filter(Laboratory.Typing.Method == "MIC (agar dilution)") %>%
+  sep_acq_mutations()
+
+micagar_cipro_acquired_sep %>%
+  upset_plot_mic_acq() +
+  labs(title = "MIC (agar dilution) measurements for acquired fluoroquinolone resistance")
+
+# MIC E-test
+micetest_cipro_acquired_sep <- cipro_res_no_mut %>%
+  filter(Laboratory.Typing.Method == "MIC (E-test gradient diffusion)")
+
+# Disk diffusion
+diskdiff_cipro_acquired_sep <- cipro_res_no_mut %>%
+  filter(Laboratory.Typing.Method == "Disk diffusion") %>%
+  sep_acq_mutations()
+
+diskdiff_cipro_acquired_sep %>%
+  upset_plot_diskdiff_acq()
 
 
 
@@ -187,13 +214,31 @@ sep_flq_mutations <- function(df) {
   require(dplyr)
   
   df1 <- df %>%
-    separate(Flq_mutations, into = c("mutation_A", "mutation_B", "mutation_C"), sep = "[;]") 
+    separate(Flq_mutations, into = c("mutation_A", "mutation_B", "mutation_C"), sep = "[;]")
   
   df2 <- df1 %>%  
     pivot_longer(cols = names(df1)[11:13],
                  values_to = "Flq_mutations") %>%
     drop_na(Flq_mutations) %>%
-    select(-name)
+    select(-name) %>%
+    relocate(Flq_mutations, .after = ST)
+  
+  return(df2)
+}
+
+# separate acquired mutations into individual mutations
+sep_acq_mutations <- function(df) {
+  require(dplyr)
+  
+  df1 <- df %>%
+    separate(Flq_acquired, into = c("acquired_A", "acquired_B", "acquired_C"), sep = "[;]")
+  
+  df2 <- df1 %>%  
+    pivot_longer(cols = names(df1)[12:14],
+                 values_to = "Flq_acquired") %>%
+    drop_na(Flq_acquired) %>%
+    select(-name) %>%
+    relocate(Flq_acquired, .after = ST)
   
   return(df2)
 }
