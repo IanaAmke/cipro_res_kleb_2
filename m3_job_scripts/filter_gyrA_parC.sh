99f02485621d3d25a367c2c5a0b50afc5ae00c6a
Iana Amke <ianaamke@gmail.com>
2022-12-31 16:26:08 +0000

    Add filter_gyrA_parC.sh

---
+++ m3_job_scripts/filter_gyrA_parC.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=filter_gyrA_parc
+#SBATCH --account=js66
+#SBATCH --time=08:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem-per-cpu=32000
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+#SBATCH --mail-type=fail
+#SBATCH --mail-user=Iana.Amke1@lshtm.ac.uk
+
+# load modules on M3
+module load python/3.9.10-linux-centos7-haswell-gcc10.2.0
+
+source /home/iamke/python_env/bin/activate
+
+# check whether all gyrA genes have been extracted
+python /home/iamke/js66_scratch/iana/extracted_seq/filter_gyrA_parC.py
+
+deactivate
+
