6ff59604e20d6b42bde5f5231adfa3dee3d75690
Iana Amke <ianaamke@gmail.com>
2022-12-31 16:25:29 +0000

    Add extract_parc_blast_mismatch.sh

---
+++ m3_job_scripts/extract_parc_blast_mismatch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=filter_gyrA_parc
+#SBATCH --account=js66
+#SBATCH --time=08:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem-per-cpu=32000
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+#SBATCH --mail-type=fail
+#SBATCH --mail-user=Iana.Amke1@lshtm.ac.uk
+
+# load modules on M3
+module load python/3.9.10-linux-centos7-haswell-gcc10.2.0
+
+source /home/iamke/python_env/bin/activate
+
+# extract mismatch sequences
+python /home/iamke/js66_scratch/iana/extracted_sequences/extract_parc_blast_mismatch.py
+
+deactivate
